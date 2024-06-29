3340a29259db92d6ea975147f7270f3bb6408c49
ayan <saifiayan822@gmail.com>
2024-06-29 22:22:08 +0530

    addFuntion

--- src/feat/c.kt
+++ src/feat/c.kt
@@ -1,2 +1,14 @@
 package feat
 
+fun operand(a:Int,b:Int,operator:(Int,Int)->Int):Int{
+
+    return operator(a,b)
+}
+
+
+fun main() {
+    println("Hello Kotlin User!")
+    var result = operand(5,5){a,b->a+b}
+    println(result)
+
+}
